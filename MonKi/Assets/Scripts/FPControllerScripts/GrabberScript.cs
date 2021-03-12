298e5c53fd00b71ebfc89a5b6b5354d423ea2708
BlackSafa <henriquerois@gmail.com>
2021-03-12 20:29:02 -0300

    Hierarquias dos códigos de movimentação

---
+++ MonKi/Assets/Scripts/FPControllerScripts/GrabberScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabberScript : PlayerMovementScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Grab()
+    {}
+
+    void Drop()
+    {}
+
+    void Throw()
+    {}
+}
--- MonKi/Assets/Scripts/FPControllerScripts/PlayerMovementScript.cs
+++ MonKi/Assets/Scripts/FPControllerScripts/PlayerMovementScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementScript : MonoBehaviour
+public class PlayerMovementScript : MouseScript
 {
 
 
