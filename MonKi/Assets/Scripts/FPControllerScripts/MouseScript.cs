8d55772cc6c52d642cfbdda6c93f367673e483fd
BlackSafa <henriquerois@gmail.com>
2021-03-12 11:57:32 -0300

    Adicionando Projeto Unity

---
+++ MonKi/Assets/Scripts/FPControllerScripts/MouseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody, cameraPosition;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    void Start()
+    {
+        //playerBody = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+        //playerBody = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        
+        yRotation += mouseX;
+
+        transform.rotation = Quaternion.Euler(xRotation, playerBody.eulerAngles.y, 0);
+        playerBody.eulerAngles = new Vector3(0, yRotation, 0);
+    }
+}
---
+++ MonKi/Assets/Scripts/FPControllerScripts/PlayerMovementScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+
+    CharacterController controller;
+    public float groundSpeed = 15f;
+    public float gravityAcceleration = 10f;
+
+    public Transform groundChecker;
+    public float groundCheckerLength = 0.4f;
+    public float jumpHeight = 3f;
+    Vector3 velocity;
+    public bool isGrounded = false;
+
+    public LayerMask groundLayer;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        Movement();
+        if (Input.GetButtonDown("Jump") && isGrounded) Jump();
+        Gravity();
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * groundSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * -gravityAcceleration);
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Gravity()
+    {
+        isGrounded = Physics.Raycast(groundChecker.position, -groundChecker.up, groundCheckerLength, groundLayer);
+        Debug.DrawLine(groundChecker.position, groundChecker.position - new Vector3(0,groundCheckerLength,0), Color.red);
+
+        if (isGrounded && velocity.y < 0) {velocity.y = -gravityAcceleration -(-1);}
+        velocity.y += -gravityAcceleration * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
