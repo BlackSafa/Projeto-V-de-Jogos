23512ddf4ec4998ac051dacb534c574673b6a264
CiscG <fgss98@gmail.com>
2021-03-26 13:57:03 -0300

    minded meio

--- MonKi/Assets/Scripts/FPSControllerScripts/Mokeys/MacacoM.cs
+++ MonKi/Assets/Scripts/FPSControllerScripts/Mokeys/MacacoM.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class MacacoM : InteracterScript
 {
-    // Start is called before the first frame update
+    public bool inmind = false;
+    public GameObject _minded;
+    
     void Start()
     {
+        PlayerStarter();
+    }
+    
+    void Update()
+    {
+        PlayerUpdate();
+    }
+   
+
+    public override Vector3 Movement()
+    {
+        Vector3 move = new Vector3();
+        if (!inmind)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+        
+           move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * groundSpeed * Time.deltaTime);
+            return (move * (groundSpeed / 1.5f));
+        }
+        else
+        {
+            return (move);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Jump()
     {
-        
+        if (!inmind)
+        {
+            base.Jump();
+        }
+    }
+
+    public void GetObjectInMind()
+    {
+        if (!isHolding)
+        {
+            
+        }
+        else
+        {
+            Drop();
+        }
+    }
+    public void MoveObjectInMind()
+    {
+
+        if (inmind)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            float z = Input.mouseScrollDelta.y;
+            Vector3 moveinair;
+            moveinair.x = x;
+            moveinair.y = y;
+            moveinair.z = z;
+            _minded.transform.position += moveinair;
+
+        }
+    }
+
+    protected override void Interact(RaycastHit hit)
+    {
+        if (!inmind)
+        {
+            base.Interact(hit);
+        }
+       
     }
 }
