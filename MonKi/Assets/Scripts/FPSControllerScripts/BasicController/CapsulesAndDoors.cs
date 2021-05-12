202d474c4254b623abcd8bb6bbf19e1e1ad33869
CiscG <fgss98@gmail.com>
2021-05-11 22:08:32 -0300

    Porta

---
+++ MonKi/Assets/Scripts/FPSControllerScripts/BasicController/CapsulesAndDoors.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsulesAndDoors : InteractiveObject
+{
+    // Start is called before the first frame update
+    private bool open, close;
+    void Start()
+    {
+        isHoldable = false;
+    }
+
+    // Update is called once per frame
+    public override void Action()
+    {
+    
+            DoorAnimation();
+       
+    }
+    
+    // ReSharper disable Unity.PerformanceAnalysis
+    void DoorAnimation()
+    {
+        if (!open)
+        {
+            GetComponent<Animator>().SetBool("Open", true);
+            open = true;
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Open", false);
+            open = false;
+        }
+    }
+}
