a5ff388887253f2f9ee5d1212a544df242d326fe
BlackSafa <henriquerois@gmail.com>
2021-06-25 20:13:25 -0300

    End (pls)

---
+++ MonKi/Assets/FinalCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class FinalCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        ChangeScene();
+    }
+
+    [PunRPC]
+    public void ChangeScene()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("Level 1");
+        }
+    }
+}
