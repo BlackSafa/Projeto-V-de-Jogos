eed2186775ef6ee1a8e9a18d65eeba74903e0b4b
CiscG <fgss98@gmail.com>
2021-07-02 17:19:04 -0300

    Att

--- MonKi/Assets/Scripts/SoundCaller.cs
+++ MonKi/Assets/Scripts/SoundCaller.cs
@@ -8,11 +8,17 @@ public class SoundCaller : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Macaco")
+        {
+            other.transform.GetComponentInParent<AudioSource>().Play();
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.tag == "Box")
+        {
+            other.gameObject.GetComponent<AudioSource>().Play();
+        }
     }
 }
