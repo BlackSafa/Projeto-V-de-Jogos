e0416a23fc720da449eae3ce09cfe551976d1971
CiscG <fgss98@gmail.com>
2021-07-09 15:07:33 -0300

    Inicio dos audio mixers e voicees

---
+++ MonKi/Assets/Scripts/Audio Controller.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private AudioMixer aMixer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     public void VolumeMaster(Slider slider)
+    {
+        switch (slider.value)
+        {
+            case 0:
+               // aMixer.SetFloat("Mu")
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+        }
+    }
+    public void VolumeMusic(Slider slider)
+    {
+        switch (slider.value)
+        {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+        }
+    }
+    public void VolumeSFX(Slider slider)
+    {
+        switch (slider.value)
+        {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+        }
+    }
+}
---
+++ MonKi/Assets/Scripts/Macacos voice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Macacosvoice : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MamacoFalaFPD()
+    {
+
+    }
+}
