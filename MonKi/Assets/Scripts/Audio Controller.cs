facf97975acc8a33ba4ab801d87d37f837aa8871
CiscG <fgss98@gmail.com>
2021-07-09 15:10:24 -0300

    Audio controller finalizado

--- MonKi/Assets/Scripts/Audio Controller.cs
+++ MonKi/Assets/Scripts/Audio Controller.cs
@@ -23,22 +23,22 @@ public class AudioController : MonoBehaviour
         switch (slider.value)
         {
             case 0:
-               // aMixer.SetFloat("Mu")
+                aMixer.SetFloat("Master", -88);
                 break;
             case 1:
-
+                aMixer.SetFloat("Master", -40);
                 break;
             case 2:
-
+                aMixer.SetFloat("Master", -20);
                 break;
             case 3:
-
+                aMixer.SetFloat("Master", -10);
                 break;
             case 4:
-
+                aMixer.SetFloat("Master", -0);
                 break;
             case 5:
-
+                aMixer.SetFloat("Master", -88);
                 break;
         }
     }
@@ -47,22 +47,22 @@ public class AudioController : MonoBehaviour
         switch (slider.value)
         {
             case 0:
-
+                aMixer.SetFloat("Music", -88);
                 break;
             case 1:
-
+                aMixer.SetFloat("Music", -40);
                 break;
             case 2:
-
+                aMixer.SetFloat("Music", -20);
                 break;
             case 3:
-
+                aMixer.SetFloat("Music", -10);
                 break;
             case 4:
-
+                aMixer.SetFloat("Music", -0);
                 break;
             case 5:
-
+                aMixer.SetFloat("Music", -88);
                 break;
         }
     }
@@ -71,22 +71,22 @@ public class AudioController : MonoBehaviour
         switch (slider.value)
         {
             case 0:
-
+                aMixer.SetFloat("SFX", -88);
                 break;
             case 1:
-
+                aMixer.SetFloat("SFX", -40);
                 break;
             case 2:
-
+                aMixer.SetFloat("SFX", -20);
                 break;
             case 3:
-
+                aMixer.SetFloat("SFX", -10);
                 break;
             case 4:
-
+                aMixer.SetFloat("SFX", -0);
                 break;
             case 5:
-
+                aMixer.SetFloat("SFX", -88);
                 break;
         }
     }
